8d0d00fd2eebc1a8a9091c4b4986f6a3853f5f89
Matheus Trinca <matheusfelipetrinca@gmail.com>
2021-05-20 18:18:09 -0300

    text and area input

--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
-import { Segment, Header, Button, FormField, Label } from 'semantic-ui-react';
+import React from 'react';
+import { Segment, Header, Button } from 'semantic-ui-react';
 import cuid from 'cuid';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { updateEvent, createEvent } from '../eventActions';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
+import MyTextInput from '../../../app/common/form/MyTextInput';
+import MyTextAreaInput from '../../../app/common/form/MyTextAreaInput';
 
 const EventForm = ({ match, history }) => {
   const selectedEvent = useSelector(state =>
@@ -23,56 +25,48 @@ const EventForm = ({ match, history }) => {
     date: '',
   };
 
-  // const handleFormSubmit = () => {
-  //   selectedEvent
-  //     ? dispatch(updateEvent({ ...selectedEvent, ...values }))
-  //     : dispatch(
-  //         createEvent({
-  //           ...values,
-  //           id: cuid(),
-  //           hostedBy: 'Bob',
-  //           attendees: [],
-  //           hostPhotoURL: '/assets/user.png',
-  //         })
-  //       );
-  //   history.push('/events');
-  // };
-
   const validationSchema = Yup.object({
     title: Yup.string().required('You must provide a title'),
+    category: Yup.string().required('You must provide a category'),
+    description: Yup.string().required(),
+    city: Yup.string().required(),
+    venue: Yup.string().required(),
+    date: Yup.string().required(),
   });
 
   return (
     <Segment clearing>
-      <Header content={selectedEvent ? 'Edit Event' : 'Create new event'} />
       <Formik
         initialValues={initialValues}
-        onSubmit={values => console.log(values)}
+        onSubmit={values => {
+          selectedEvent
+            ? dispatch(updateEvent({ ...selectedEvent, ...values }))
+            : dispatch(
+                createEvent({
+                  ...values,
+                  id: cuid(),
+                  hostedBy: 'Bob',
+                  attendees: [],
+                  hostPhotoURL: '/assets/user.png',
+                })
+              );
+          history.push('/events');
+        }}
         validationSchema={validationSchema}
       >
         <Form className="ui form">
-          <FormField>
-            <Field name="title" placeholder="Event Title" />
-            <ErrorMessage
-              name="title"
-              render={error => <Label basic color="red" content={error} />}
-            />
-          </FormField>
-          <FormField>
-            <Field name="category" placeholder="Category" />
-          </FormField>
-          <FormField>
-            <Field name="description" placeholder="Description" />
-          </FormField>
-          <FormField>
-            <Field name="city" placeholder="City" />
-          </FormField>
-          <FormField>
-            <Field name="venue" placeholder="Venue" />
-          </FormField>
-          <FormField>
-            <Field name="date" placeholder="Date" type="date" />
-          </FormField>
+          <Header sub color="teal" content="Event Details" />
+          <MyTextInput placeholder="Event Title" name="title" />
+          <MyTextInput placeholder="Event Category" name="category" />
+          <MyTextAreaInput
+            placeholder="Description"
+            name="description"
+            rows={2}
+          />
+          <Header sub color="teal" content="Event Location Details" />
+          <MyTextInput placeholder="City" name="city" />
+          <MyTextInput placeholder="Venue" name="venue" />
+          <MyTextInput placeholder="Event Date" name="date" type="date" />
 
           <Button type="submit" floated="right" positive content="Submit" />
           <Button as={Link} to="/events" floated="right" content="Cancel" />
