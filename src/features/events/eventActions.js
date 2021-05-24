02459e8cb5e0d2dad0b4602ddc86f5d608b2da83
MatheusTrinca <matheusfelipetrinca@gmail.com>
2021-05-24 20:37:28 -0300

    firestore fetch data

--- src/features/events/eventActions.js
+++ src/features/events/eventActions.js
@@ -22,6 +22,13 @@ export const loadEvents = () => async dispatch => {
   }
 };
 
+export const listenToEvents = events => {
+  return {
+    type: FETCH_EVENTS,
+    payload: events,
+  };
+};
+
 export const createEvent = event => {
   return {
     type: CREATE_EVENT,
--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Grid } from 'semantic-ui-react';
 import EventFilters from './EventFilters';
 import EventList from './EventList';
 import EventListPlaceholder from './EventListPlaceholder';
+import {
+  getEventsFromFirestore,
+  dataFromSnapshot,
+} from '../../../app/firestore/firestoreService';
+import { useDispatch } from 'react-redux';
+import { listenToEvents } from '../eventActions';
+import {
+  asyncActionStart,
+  asyncActionFinish,
+  asyncActionError,
+} from '../../../app/async/asyncReduces';
 
 function EventDashboard() {
   const { events } = useSelector(state => state.event);
   const { loading } = useSelector(state => state.async);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(asyncActionStart());
+    const unsubscribe = getEventsFromFirestore({
+      next: snapshot => {
+        dispatch(
+          listenToEvents(
+            snapshot.docs.map(docSnapshot => dataFromSnapshot(docSnapshot))
+          )
+        );
+        dispatch(asyncActionFinish());
+      },
+      error: error => dispatch(asyncActionError(error)),
+      complete: console.log('You will never see this messege'),
+    });
+    return unsubscribe;
+  }, [dispatch]);
 
   return (
     <Grid>
