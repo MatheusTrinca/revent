c525ac8dd6b345eca7c324044a90b936974c1ed7
Matheus Felipe Trinca <matheusfelipetrinca@gmail.com>
2021-05-28 00:03:18 -0300

    profile contents

--- src/features/profiles/profilePage/ProfilePage.jsx
+++ src/features/profiles/profilePage/ProfilePage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
 import ProfileHeader from './ProfileHeader';
+import ProfileContent from './ProfileContent';
 
 export default function ProfilePage() {
   return (
     <Grid>
       <Grid.Column width="16">
         <ProfileHeader />
-        <h2>Profile Content</h2>
+        <ProfileContent />
       </Grid.Column>
     </Grid>
   );
