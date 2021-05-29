8119679ff572ba3131428fe8083746a13032891b
Matheus Trinca <matheusfelipetrinca@gmail.com>
2021-05-28 21:33:48 -0300

    user profile

--- src/features/profiles/profilePage/AboutTab.jsx
+++ src/features/profiles/profilePage/AboutTab.jsx
@@ -3,18 +3,24 @@ import { Tab, Grid, Header, Button } from 'semantic-ui-react';
 import { format } from 'date-fns';
 import ProfileForm from './ProfileForm';
 
-export default function AboutTab({ profile }) {
+export default function AboutTab({ profile, isCurrentUser }) {
   const [editMode, setEditMode] = useState(false);
   return (
     <Tab.Pane>
       <Grid>
         <Grid.Column width={16}>
-          <Header floated="left" icon="user" content={`About ${profile.displayName}`} />
-          <Button
-            floated="right"
-            content={editMode ? 'Cancel' : 'Edit'}
-            onClick={() => setEditMode(!editMode)}
+          <Header
+            floated="left"
+            icon="user"
+            content={`About ${profile.displayName}`}
           />
+          {isCurrentUser && (
+            <Button
+              floated="right"
+              content={editMode ? 'Cancel' : 'Edit'}
+              onClick={() => setEditMode(!editMode)}
+            />
+          )}
         </Grid.Column>
         <Grid.Column width={16}>
           {editMode ? (
@@ -22,7 +28,9 @@ export default function AboutTab({ profile }) {
           ) : (
             <>
               <div style={{ marginBottom: 10 }}>
-                <strong>Member since: {format(profile.createdAt, 'dd MMM yyyy')}</strong>
+                <strong>
+                  Member since: {format(profile.createdAt, 'dd MMM yyyy')}
+                </strong>
                 <div>{profile.description || null}</div>
               </div>
             </>
--- src/features/profiles/profilePage/ProfileHeader.jsx
+++ src/features/profiles/profilePage/ProfileHeader.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import { Segment, Grid, Item, Header, Statistic, Divider, Reveal, Button } from 'semantic-ui-react';
+import {
+  Segment,
+  Grid,
+  Item,
+  Header,
+  Statistic,
+  Divider,
+  Reveal,
+  Button,
+} from 'semantic-ui-react';
 
-export default function ProfileHeader({ profile }) {
+export default function ProfileHeader({ profile, isCurrentUser }) {
   return (
     <Segment>
       <Grid>
         <Grid.Column width={12}>
           <Item.Group>
             <Item>
-              <Item.Image avatar size="small" src={profile.photoURL || '/assets/user.png'} />
+              <Item.Image
+                avatar
+                size="small"
+                src={profile.photoURL || '/assets/user.png'}
+              />
               <Item.Content verticalAlign="middle">
                 <Header
                   as="h1"
@@ -24,15 +37,19 @@ export default function ProfileHeader({ profile }) {
             <Statistic label="Followers" value={10} />
             <Statistic label="Following" value={5} />
           </Statistic.Group>
-          <Divider />
-          <Reveal animated="move">
-            <Reveal.Content visible style={{ width: '100%' }}>
-              <Button fluid color="teal" content="Following" />
-            </Reveal.Content>
-            <Reveal.Content hidden style={{ width: '100%' }}>
-              <Button basic fluid color="red" content="Unfollow" />
-            </Reveal.Content>
-          </Reveal>
+          {!isCurrentUser && (
+            <>
+              <Divider />
+              <Reveal animated="move">
+                <Reveal.Content visible style={{ width: '100%' }}>
+                  <Button fluid color="teal" content="Following" />
+                </Reveal.Content>
+                <Reveal.Content hidden style={{ width: '100%' }}>
+                  <Button basic fluid color="red" content="Unfollow" />
+                </Reveal.Content>
+              </Reveal>
+            </>
+          )}
         </Grid.Column>
       </Grid>
     </Segment>
